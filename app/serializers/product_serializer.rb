5bcb6b7b70516def29c0356be1a8368e8c0049c4
Eric McKinney <mckinney99@gmail.com>
2018-03-14 01:58:39 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,9 +1,7 @@
 class ProductSerializer
-
     def self.serialize(product)
 
     serialized_product = '{'
-
     serialized_product += '"id": ' + product.id.to_s + ', '
     serialized_product += '"name": "' + product.name + '", '
     serialized_product += '"price": "' + product.price.to_s + '", '
